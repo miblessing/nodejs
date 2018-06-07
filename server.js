5db5a4689fb64eafb456417059cbe69fa5e72007
Michelle Blessing <micblessing@gmail.com>
2018-06-07 09:04:40 -0700

    Add projects page

--- server.js
+++ server.js
@@ -49,13 +49,18 @@ app.get('/', (req, res) => {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the Home Page'
   });
-
 });
 
 app.get('/about', (req, res) => {
   // res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
   });
 });
 
